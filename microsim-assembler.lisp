c615ec3eaf9eb17c599053de1ec91e013be8cab8
Barry Bridgens <barry@bridgens.me.uk>
2022-12-06 20:20:36 +0000

    LDA_M op-code added - Address handling corrected

--- microsim-assembler.lisp
+++ microsim-assembler.lisp
@@ -3,20 +3,29 @@
 (in-package #:microsim-assembler)
 
 
-(defparameter *valid-op-codes* '("LDA_I" "JMP"))
+(defparameter *valid-op-codes* '("LDA_I" "LDA_M" "JMP"))
 
 (defparameter *line-cnt* 0)
 (defparameter *address* #x100)
 (defparameter *labels* ())
 (defparameter *output* ())
 
-(defun initialise ())
+(defun initialise ()
+  (setq *line-cnt* 0)
+  (setq *address* #x100)
+  (setq *labels* ())
+  (setq  *output* ()))
 
 (defun parse-hex (s)
   (if (string= (str:substring 0 2 s) "0x")
       (return-from parse-hex (parse-integer (str:substring 2 t s) :radix 16)))
   (if (string= (str:substring 0 1 s) "$")
       (return-from parse-hex (parse-integer (str:substring 1 t s) :radix 16))))
+
+(defun prepend-and-inc-address(n)
+  (let ((old-address *address*))
+	(setq *address* (+ *address* 1))
+	(return-from prepend-and-inc-address (list old-address n))))
 
 (defun process-label (s)
   (format t "~a -> LABEL~%" s)
@@ -29,21 +38,30 @@
     (cond
       ((str:starts-with-p "LDA_I" s)
        (process-op-code-lda_i tokens))
+      ((str:starts-with-p "LDA_M" s)
+       (process-op-code-lda_m tokens))
       ((str:starts-with-p "JMP" s)
        (process-op-code-jmp tokens))))
   )
 
 (defun process-op-code-lda_i (tokens)
-  (format t "LDAI ================ ~a~%" (second tokens))
-  (setq *output* (append *output* '(1)))
-  (setq *output* (append *output* (list (parse-integer (second tokens))))))
+  (format t "LDA_I ================ ~a~%" (second tokens))
+  (setq *output* (append *output* (prepend-and-inc-address '1)))
+  (setq *output* (append *output* (prepend-and-inc-address (parse-integer (second tokens))))))
+
+(defun process-op-code-lda_m (tokens)
+  (format t "LDA_M ================ ~a~%" (second tokens))
+  (setq *output* (append *output* (prepend-and-inc-address '2)))
+  (let ((addr (cdr (assoc (second tokens) *labels* :test #'equalp))))
+    (setq *output* (append *output* (prepend-and-inc-address (nth-value 0 (truncate addr 256)))))
+    (setq *output* (append *output* (prepend-and-inc-address (nth-value 1 (truncate addr 256)))))))
 
 (defun process-op-code-jmp (tokens)
   (format t "JMP ================ ~a~%" (second tokens))
-  (setq *output* (append *output* '(10)))
+  (setq *output* (append *output* (prepend-and-inc-address '10)))
   (let ((addr (cdr (assoc (second tokens) *labels* :test #'equalp))))
-    (setq *output* (append *output* (list (nth-value 0 (truncate 528 256)))))
-    (setq *output* (append *output* (list (nth-value 1 (truncate 528 256)))))))
+    (setq *output* (append *output* (prepend-and-inc-address (nth-value 0 (truncate addr 256)))))
+    (setq *output* (append *output* (prepend-and-inc-address (nth-value 1 (truncate addr 256)))))))
 
 (defun process-org (s)
   (format t "~a -> ORG ~%" s)
@@ -53,6 +71,7 @@
     (format t "  Address is ~x  /  ~d ~%" *address* *address*)))
 
 (defun parse-asm-file ()
+  (initialise)
   (let ((in (open "/home/barry/software/projects/microsim-assembler/test.asm" :if-does-not-exist nil)))
   (when in
     (loop for line = (read-line in nil)
--- test.asm
+++ test.asm
@@ -1,10 +1,15 @@
 	# Test file for microsim-assembler
 
 	ORG 0x200
-
+var1:
+	
 	ORG $210
 	
 start:	
 	LDA_I	27
+
+	ORG $220
+	
+	LDA_M	var1
 	JMP	start
 	
